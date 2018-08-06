4b0f16ea446c2ab9a39cc94f3cfa7594a8c6a763
Yuki Kaito <ykaito21@gmail.com>
2018-08-06 19:46:14 +0900

    add stripe gem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
+require 'json'
+require 'open-uri'
+
 class PagesController < ApplicationController
   def home
+    url = 'http://zipcloud.ibsnet.co.jp/api/search?zipcode=2160007'
+    data = open(url).read
+    results = JSON.parse(data)
+    @addresses = results['results']
   end
 end
